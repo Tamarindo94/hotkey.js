211a89f774d0fbb5a2eb988232e9e76d1f99b227
Tamarindo94 <askeron@libero.it>
2023-12-06 00:04:25 +0100

    fix

--- hotkey.js
+++ hotkey.js
@@ -1,13 +1,27 @@
-function setHotkey(cmd, callback, override=true) {
-	let tokens = cmd.replace("++", "+plus").toLowerCase().split("+")
-	let ctrl = splouse(tokens, "ctrl")
-	let shift = splouse(tokens, "shift")
-	let alt = splouse(tokens, "alt")
+// Only single hotkeys are supported, with supported modifiers ctrl, shoft, alt
+function setHotkey(cmd, callback, target, override=true) {
+	let tokens = cmd.replace("++", "+plus").toLowerCase().split("+").map()
+	let ctrl = splouseBool(tokens, "ctrl")
+	let shift = splouseBool(tokens, "shift")
+	let alt = splouseBool(tokens, "alt")
+	console.log(tokens)
+	let keyCode = tokens[0].charCodeAt(0)
+	;["keydown", "keyup", "keypress"].forEach( evType => {
+		(target || window).addEventListener(evType, e => {
+			if(ctrl !== e.ctrlKey || shift !== e.shiftKey || alt !== e.altKey || keyCode !== e.keyCode) return
+			if(override) { e.preventDefault(); e.stopImmediatePropagation(); e.stopPropagation() }
+			callback()
+		}, true)
+	})
 }
 
-function splouse(arr, el) {
-	let id = arr.findIndex( v => v === el )
-	if(id === -1) return false
-	arr.splice(id, 1)
-	return true
+function splouse(arr, startEl, ct=1) {
+	let id = arr.findIndex( v => v === startEl )
+	if(id === -1) return arr
+	arr.splice(id, ct)
+	return arr
+}
+
+function splouseBool(arr, startEl, ct=1) {
+	return arr.length !== splouse(arr, startEl, ct).length
 }
