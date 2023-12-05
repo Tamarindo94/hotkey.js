9f6533265a67c26ee6f700b06808e8eba94064f6
Tamarindo94 <askeron@libero.it>
2023-12-06 00:54:51 +0100

    fix

--- hotkey.js
+++ hotkey.js
@@ -1,23 +1,26 @@
 // Only single hotkeys are supported, with supported modifiers ctrl, shoft, alt
-function setHotkey(cmd, callback, target=window, override=true) {
-	let tokens = cmd.replace("++", "+plus").toLowerCase().split("+").map( tok => tok.trim() )
-	let ctrl = splouseBool(tokens, "ctrl")
-	let shift = splouseBool(tokens, "shift")
-	let alt = splouseBool(tokens, "alt")
-	let keyCode = tokens[0].charCodeAt(0)
-	if(e.key === ":") e.keyCode = 190
-	if(e.key === ";") e.keyCode = 188
-	console.log("ctrl", ctrl, "shift", shift, "alt", alt, "keyCode outside", keyCode, "target", target)
-	;["keydown", "keyup", "keypress"].forEach( evType => {
-		target.addEventListener(evType, (e) => {
-			console.log("ctrl", ctrl, "shift", shift, "alt", alt, "keyCode", e.keyCode, "key", e.key)
-			if(ctrl !== e.ctrlKey || shift !== e.shiftKey || alt !== e.altKey || keyCode !== e.keyCode) return
-			if(override) { e.preventDefault(); e.stopPropagation(); e.stopImmediatePropagation() }
-			console.log("keycode inside", e.keyCode, e.key)
-			if(e.type === "keyup" && callback) callback(e, cmd)
-		}, true)
-	})
-	console.log("hotkey " + cmd + " seth")
+function setHotkey(commands, callback, target=window, override=true) {
+	commands = Array.isArray(commands) ? commands : [commands]
+	commands.forEach( cmd => {
+		let tokens = cmd.replace("++", "+plus").toLowerCase().split("+").map( tok => tok.trim() )
+		let ctrl = splouseBool(tokens, "ctrl")
+		let shift = splouseBool(tokens, "shift")
+		let alt = splouseBool(tokens, "alt")
+		let keyCode = tokens[0].charCodeAt(0)
+		if(keyCode === 110) keyCode = 190
+		if(keyCode === 108) e.keyCode = 188
+		console.log("ctrl", ctrl, "shift", shift, "alt", alt, "keyCode outside", keyCode, "target", target)
+		;["keydown", "keyup", "keypress"].forEach( evType => {
+			target.addEventListener(evType, (e) => {
+				console.log("ctrl", ctrl, "shift", shift, "alt", alt, "keyCode", e.keyCode, "key", e.key)
+				if(ctrl !== e.ctrlKey || shift !== e.shiftKey || alt !== e.altKey || keyCode !== e.keyCode) return
+				if(override) { e.preventDefault(); e.stopPropagation(); e.stopImmediatePropagation() }
+				console.log("keycode inside", e.keyCode, e.key)
+				if(e.type === "keyup" && callback) callback(e, cmd)
+			}, true)
+		})
+		console.log("hotkey " + cmd + " sethi")
+	}
 }
 
 function splouse(arr, startEl, ct=1) {
