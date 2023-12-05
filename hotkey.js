e09c3108eeb717a012f6201fb9e8255cbdcc455c
Tamarindo94 <askeron@libero.it>
2023-12-06 00:24:22 +0100

    fix

--- hotkey.js
+++ hotkey.js
@@ -15,6 +15,7 @@ function setHotkey(cmd, callback, target=window, override=true) {
 			if(e.type === "keyup" && callback) callback(e, cmd)
 		}, true)
 	})
+	console.log("hotkey " + cmd + " set")
 }
 
 function splouse(arr, startEl, ct=1) {
