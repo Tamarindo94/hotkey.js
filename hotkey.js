f705c3957b4f424e8406eb4e08ad2178d4968bec
Tamarindo94 <askeron@libero.it>
2023-12-06 00:09:23 +0100

    fix

--- hotkey.js
+++ hotkey.js
@@ -10,7 +10,7 @@ function setHotkey(cmd, callback, target, override=true) {
 		;(target || window).addEventListener(evType, e => {
 			if(ctrl !== e.ctrlKey || shift !== e.shiftKey || alt !== e.altKey || keyCode !== e.keyCode) return
 			if(override) { e.preventDefault(); e.stopImmediatePropagation(); e.stopPropagation() }
-			callback()
+			if(callback) callback()
 		}, true)
 	})
 }
