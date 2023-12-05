f3c1509e713c89f9badee818445e8cba0969ca18
Tamarindo94 <askeron@libero.it>
2023-12-05 23:50:10 +0100

    fix

--- hotkey.js
+++ hotkey.js
@@ -1,14 +1,13 @@
 function setHotkey(cmd, callback, override=true) {
 	let tokens = cmd.replace("++", "+plus").toLowerCase().split("+")
-	console.log(tokens)
 	let ctrl = splouse(tokens, "ctrl")
-	console.log(tokens)
 	let shift = splouse(tokens, "shift")
-	console.log(tokens)
 	let alt = splouse(tokens, "alt")
-	console.log(tokens)
 }
 
 function splouse(arr, el) {
-	return arr.splice(arr.findIndex( v => v === el ), 1)
+	let id = arr.findIndex( v => v === el )
+	if(id === -1) return false
+	arr.splice(id, 1)
+	return true
 }
