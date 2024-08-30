0a16ae5172da78928387e0d1a8bc0046cbecf727
Tamarindo94 <askeron@libero.it>
2024-08-30 10:47:34 +0200

    Update hotkey.js

--- hotkey.js
+++ hotkey.js
@@ -4,7 +4,8 @@ const defaultOpts = {
 	target: window,
 	triggers: "keyup",
 	override: true,
-	once: false
+	once: false,
+	log: false
 }
 
 // Can only bind one key, with modifiers ctrl, shift, alt: shift+A is ok, ctrl+A+B is not
@@ -19,19 +20,19 @@ function setHotkeys(commands, callback, opts) {
 		let boundKey = _MAPS["common"][tokens[0]] || _MAPS[_LOCALE][tokens[0]] || tokens[0].toUpperCase().charCodeAt(0)
 		// console.log("ctrl", ctrl, "shift", shift, "alt", alt, "boundKey outside", boundKey, "target", target)
 		const hotkeyCb = (e) => {
-			// console.log("boundkey = " + boundKey + ", pressed: " + e.keyCode + ", e: " + e.type)
-			// console.log("ctrl", ctrl, "shift", shift, "alt", alt, "boundKey inside", boundKey, "key", e.keyCode)
+			if(opts.log) console.log("boundkey = " + boundKey + ", pressed: " + e.keyCode + ", e: " + e.type)
+			if(opts.log) console.log("ctrl", ctrl, "shift", shift, "alt", alt, "boundKey inside", boundKey, "key", e.keyCode)
 			if(boundKey !== e.keyCode || ctrl !== e.ctrlKey || shift !== e.shiftKey || alt !== e.altKey) return
 			if(opts.override) suppressEvent(e)
 			if(opts.triggers.includes(e.type)) {
-				// console.log("Trigger for " + cmd)
+				if(opts.log) console.log("Trigger for " + cmd)
 				if(callback) callback(e, cmd)
 				if(!opts.once) return
 				keyEvTypes.forEach( evType => opts.target.removeEventListener(evType, hotkeyCb, {capture:true}) )
 			}
 		}
 		keyEvTypes.forEach( evType => opts.target.addEventListener(evType, hotkeyCb, true) ) // use capture			
-		// console.log("hotkey " + cmd + " set")
+		if(opts.log) console.log("hotkey " + cmd + " set")
 	})
 }
 
